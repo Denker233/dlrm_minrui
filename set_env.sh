a26580130d5b3f7b282a20a96a1de150859e21b4
Denker233 <69044597+Denker233@users.noreply.github.com>
2024-11-19 13:21:19 -0500

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -7,12 +7,12 @@ sudo apt install -y python3.9-venv
 
 python3.9 -m venv dlrm_env
 
-source dlrm_env/bin/activate
+# source dlrm_env/bin/activate
 
-export TMPDIR=$PWD/dlrm_env
+# export TMPDIR=$PWD/dlrm_env
 
-#change the route to location of dlrm
-cd "$PWD/dlrm_minrui"
+# #change the route to location of dlrm
+# cd "$PWD/dlrm_minrui"
 
-pip3 install -r requirements.txt --no-cache-dir
-pip3 install tensorboard mlperf_logging
+# pip3 install -r requirements.txt --no-cache-dir
+# pip3 install tensorboard mlperf_logging
