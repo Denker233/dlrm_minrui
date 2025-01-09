f952ebf4c793c06b68e2a1f051be4c71cd3b1d49
Denker233 <69044597+Denker233@users.noreply.github.com>
2025-01-08 19:59:07 -0500

    Update kaggle_throttle.sh

--- kaggle_throttle.sh
+++ kaggle_throttle.sh
@@ -44,7 +44,7 @@ frequencies=( 2.0 1.6 1.4 1 0.6 0.2)  # Frequencies in GHz
 for freq in "${frequencies[@]}"; do
     # Apply CPU frequency to NUMA node 1 (CPUs 10-19, 30-39)
     echo "Setting CPU frequency of NUMA 1 CPUs (10-19, 30-39) to ${freq} GHz"
-    sudo bash /users/mt1370/throttle/run_freq.sh 2.4 ${freq} 10
+    sudo bash /users/mt1370/throttle/scripts/run_freq.sh 2.4 ${freq} 10
 
 
 
@@ -56,7 +56,7 @@ for freq in "${frequencies[@]}"; do
     --dataset-multiprocessing --data-generation=dataset --data-set=kaggle --raw-data-file=./input/train.txt \
     --processed-data-file=./input/kaggleAdDisplayChallenge_processed.npz --loss-function=bce \
     --round-targets=True --learning-rate=0.1 --mini-batch-size=128 --print-freq=8192 --print-time \
-    --test-mini-batch-size=16384 --test-num-workers=16 --enable-profiling $dlrm_extra_option\
+    --test-mini-batch-size=16384 --test-num-workers=16 $dlrm_extra_option\
     >> combined_output_numa1_10GB.log 2>&1 &
 
     python_pid=$!
